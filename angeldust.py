4f758899f54735854252beb9afffb0c674007130
Anders Pearson <anders@columbia.edu>
2010-11-12 16:36:30 -0500

    initial commit

---
+++ angeldust.py
@@ -0,0 +1,106 @@
+#!ve/bin/python
+
+import cStringIO
+import plistlib
+from pprint import pprint
+from restclient import POST,GET
+import os.path
+import sys
+import os
+
+class PCP:
+    def __init__(self,base_url,username,password):
+        self.BASE = base_url
+        self.credentials = (username,password)
+        self.session = None
+        
+    def get_session(self):
+        if not self.session:
+            # need to login
+            resp,contents = POST(self.BASE + "capture/login",
+                                 params =                              {
+                    'authenticating_user' : self.credentials[0],
+                    'commit' : 'Log In',
+                    'password' : self.credentials[1],
+                    },
+                                 headers = {'X-Requested-With' : 'XMLHttpRequest'},
+                                 credentials=self.credentials,
+                                 resp = True,
+                                 async=False)
+
+            cookie_string = resp['set-cookie']
+            #quick and dirty parsing
+            self.session = cookie_string.split(";")[0].split("=")[1]
+        return self.session
+
+    def workflows(self):
+        content = GET(self.BASE + "workflows",
+                      credentials=self.credentials)
+        s = cStringIO.StringIO(content)
+        p = plistlib.readPlist(s)
+        return p['workflows']
+
+    def select_workflow(self,uuid):
+        POST(self.BASE + "capture/select_workflow",
+             params=dict(selected_workflow=uuid),
+             credentials=self.credentials,
+             headers={'Cookie' : "_session_id=" + self.get_session() + "; BALANCEID=balancer.mongrel2"},
+             async=False)
+
+    def upload_file(self,filepath,workflow_uuid,title="",description=""):
+        # BIG GOTCHA HERE:
+        # the workflow is set in a separate request from the upload
+        # and state is maintained on the server. 
+        # that means that there's a race condition here if two PCP
+        # objects with the same session_id try to upload files
+        # to different workflows at roughly the same time.
+        # if there's any chance at all of that happening, you want to 
+        # put some kind of mutex around this method call
+
+        # we have to set the workflow uuid first, in a separate request
+        self.select_workflow(workflow_uuid)
+        # now we can actually do the upload
+        params = dict(
+            title=title,
+            workflow_select=workflow_uuid, # probably redundant
+            description=description)
+        content = POST(self.BASE + "capture/file_upload",
+                       params=params,
+                       files={'source_file' : 
+                              {'file' : open(filepath).read(),
+                               'filename' : os.path.basename(filepath)}},
+                       credentials=self.credentials,
+                       headers={'Cookie' : "_session_id=" + self.get_session() + "; BALANCEID=balancer.mongrel2"},
+                       async=False
+                       )
+        # if it succeeds, it doesn't actually return anything
+
+if __name__ == "__main__":
+    # some simple command line stuff for testing
+    username = os.environ.get('ANGELDUST_USERNAME',"")
+    password = os.environ.get('ANGELDUST_PASSWORD',"")
+    if username == "" or password == "":
+        print "you must set ANGELDUST_USERNAME and ANGELDUST_PASSWORD environment variables"
+        sys.exit(1)
+    
+    base_url = os.environ.get('ANGELDUST_BASE_URL',"")
+    pcp = PCP(base_url,username,password)
+
+    command = sys.argv[1]
+    if command == "list_workflows":
+        # ./angeldust.py list_workflows
+        for wf in pcp.workflows():
+            print wf['uuid'],wf['title']
+    elif command == "upload_file":
+        # ./angeldust.py upload_file /path/to/file.py workflow_uuid title description
+        filename = sys.argv[2]
+        uuid = sys.argv[3]
+        title = sys.argv[4]
+        description = sys.argv[5]
+        pcp.upload_file(filename,uuid,title,description)
+    else:
+        print "unknown command"
+
+
+
+
